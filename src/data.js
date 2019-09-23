58f6abcdffa115e08b8ad985ade396879cdf6066
ConstanzaCastro <redlight.down@gmail.com>
2019-09-23 17:02:45 -0300

    variables data

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ window.dataPokemon=dataPokemon;
  
 const filterByType = (dataPokemon, filterValue) => {
     
-    const resultType = pokemonList.filter(Element => {
+    const resultType = dataPokemon.filter(Element => {
         return Element.type.includes(filterValue);
         
     });
